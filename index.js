0c0eda8123d7e63b30696bea1cfa4396b9608a3a
ilyamixaltik <mixaltik16@gmail.com>
2021-06-25 13:41:59 +0300

    create index router

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+app.get('/', require('./routes/index'))
+
 app.listen(port, () => {
     console.log(`Server started successfully to port ${port}`)
 })
