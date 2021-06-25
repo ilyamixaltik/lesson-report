d69ff59a1524ed17fbc3e8dea93f793e93e364f7
ilyamixaltik <mixaltik16@gmail.com>
2021-06-25 15:28:02 +0300

    create config file

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    urlPostgreSQL: 'postgres://postgres:postgres@127.0.0.1:5432/test',
+    expressPort: 3000
+}
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require('sequelize');
 const express = require('express')
 const app = express()
-const port = 3000
+const config = require('./config')
 
-const sequelize = new Sequelize('postgres://postgres:postgres@127.0.0.1:5432/test')
-sequelize.connectionManager.getConnection('postgres://postgres:postgres@127.0.0.1:5432/test')
+const sequelize = new Sequelize(config.urlPostgreSQL)
+sequelize.connectionManager.getConnection(config.urlPostgreSQL)
     .then(async () => console.log('[PostgreSQL] База данных успешно запущена'))
     .catch(async (e) => console.error)
 
 app.get('/', require('./routes/index'))
 
-app.listen(port, () => {
-    console.log(`Server started successfully to port ${port}`)
+app.listen(config.expressPort, () => {
+    console.log(`[Express] Сервер успешно запущен`)
 })
