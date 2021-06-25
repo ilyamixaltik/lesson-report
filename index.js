340352864687a8e4f0b75a3c1dece9f7a0996132
ilyamixaltik <mixaltik16@gmail.com>
2021-06-25 15:18:03 +0300

    create connection to database

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const { Sequelize } = require('sequelize');
 const express = require('express')
 const app = express()
-const port = 8000
+const port = 3000
+
+const sequelize = new Sequelize('postgres://postgres:postgres@127.0.0.1:5432/test')
+sequelize.connectionManager.getConnection('postgres://postgres:postgres@127.0.0.1:5432/test')
+    .then(async () => console.log('[PostgreSQL] База данных успешно запущена'))
+    .catch(async (e) => console.error)
 
 app.get('/', require('./routes/index'))
 
